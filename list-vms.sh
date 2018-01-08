c94fe13a8fca7880f4bed8fcaf8ddd1378ad1613
Charlie Drage <charlie@charliedrage.com>
2018-01-08 15:15:56 -0500

    Add list vms script

---
+++ list-vms.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+list_all_vms() {
+  output="NAME IP\n"
+  vms=`sudo virsh list | awk '{ print $2}' | tail -n +3`
+  for VM in $vms
+  do
+    VM_IP=$(for mac in `sudo virsh domiflist $VM |grep -o -E "([0-9a-f]{2}:){5}([0-9a-f]{2})"` ; do sudo arp -e |grep $mac  |grep -o -P "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" ; done)
+    output+="$VM $VM_IP"
+  done
+
+  echo -e $output | column -t
+}
+
+list_all_vms
