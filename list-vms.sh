1c1cc9364c38570aab233c5f70069b46436a0c0e
Charlie Drage <charlie@charliedrage.com>
2018-01-08 15:34:14 -0500

    update

--- list-vms.sh
+++ list-vms.sh
@@ -2,11 +2,14 @@
 
 list_all_vms() {
   output="NAME IP\n"
-  vms=`sudo virsh list | awk '{ print $2}' | tail -n +3`
+  vms=`sudo virsh list --state-running | awk '{ print $2}' | tail -n +3`
   for VM in $vms
   do
     VM_IP=$(for mac in `sudo virsh domiflist $VM |grep -o -E "([0-9a-f]{2}:){5}([0-9a-f]{2})"` ; do sudo arp -e |grep $mac  |grep -o -P "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" ; done)
-    output+="$VM $VM_IP"
+    if [ -z "$VM_IP" ]; then
+      VM_IP="NOTFOUND"
+    fi
+    output+="$VM $VM_IP\n"
   done
 
   echo -e $output | column -t
@@ -16,4 +19,4 @@ list_all_vms
 
 echo ""
 echo "Note: If an IP address is blank. Use nmap and re-run list-vms.sh"
-echo "ex. arp -sn 192.168.1.0/24"
+echo "ex. nmap -sn 192.168.1.0/24"
